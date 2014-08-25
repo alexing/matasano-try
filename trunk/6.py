fc8f88a0e1d8e5661617187e06519d82c58e0976
alexing10 <alexing10@c6c7d0f9-ba2c-60ac-e04f-15740ba22bec>
2014-08-25 15:01:27 +0000

    6) blocks

--- trunk/6.py
+++ trunk/6.py
@@ -34,4 +34,38 @@ f=open("out.txt","w+")
 
 lines=(base64.b64decode(lines)).encode("hex")
 f.write(lines)
+
+bestScore=1000 #digamos
+for keySize in xrange(2,40):
+
+    #distancias
+    dAB=hammingDistance(lines[0:keySize],lines[keySize:keySize*2])
+    dAC=hammingDistance(lines[0:keySize],lines[keySize*2:keySize*3])
+    dAD=hammingDistance(lines[0:keySize],lines[keySize*3:keySize*4])
+    dBC=hammingDistance(lines[keySize:keySize*2],lines[keySize*2:keySize*3])
+    dBD=hammingDistance(lines[keySize:keySize*2],lines[keySize*3:keySize*4])
+    dCD=hammingDistance(lines[keySize*2:keySize*3],lines[keySize*3:keySize*4])
+    
+    #promedios
+    aAB=dAB/keySize
+    aAC=dAC/keySize
+    aAD=dAD/keySize
+    aBC=dBC/keySize
+    aBD=dBD/keySize
+    aCD=dCD/keySize
+    #puntajes
+    score=(aAB+aAC+aAD+aBC+aBD+aCD)/6
+
+    if score<bestScore:
+        bestScore=score
+        bestKeySize=keySize
+
+#print str(bestKeySize)+": "+ str(bestScore)
+
+blocks=[]
+for i in xrange(0, len(lines), bestKeySize):
+       
+    blocks.append(lines[i:i+bestKeySize])
+    #print lines[i:i+bestKeySize]
+
 f.close()
