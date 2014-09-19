748a080c287d34771d4fe5b8afadaa14e5c6776d
alexing10 <alexing10@c6c7d0f9-ba2c-60ac-e04f-15740ba22bec>
2014-09-19 15:24:12 +0000

    Mejoré el 3, para ir a atacar el 6 de manera distinta. Implemente un ataque estadistico. Agregué un script llamado "freq" que mide la frencuencia de caracteres en una base de textos. Se construye un directorio "txts" en la carpeta raiz y ahi se vuelca la base deseada. Lo corri contra 200mb de plaintxts, que obviamente no commitie.

--- trunk/3.py
+++ trunk/3.py
@@ -2,21 +2,25 @@
 import binascii
 import string
 from cryp import hexToBin, extendIntInBin, XOR
+from collections import Counter
+
+mostPossibleCharacters=[' ','e','t','a','o','i','n']
 
 encoded="1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 encodedBin=hexToBin(encoded)
 
-f=open("out.txt","w+")
+#What's the most repeated byte in the encoded string?
+myCounter=Counter(encodedBin[i:i+8] for i in range(0,len(encodedBin),8))
+mostRepeatedChar=myCounter.most_common()[0][0]
 
-for i in xrange(0x00,0xFF):
-	auxString=extendIntInBin(i,len(encodedBin))
+#C=p*k
+#Got C, we know possible P's... so let's find possible K's
+for i in mostPossibleCharacters:
+	possibleKey=int(binascii.hexlify(i), 16)^int(mostRepeatedChar,2)
+	auxString=extendIntInBin(possibleKey,len(encodedBin))
 	xor=XOR(auxString,encodedBin,2)
-	if len(xor)%2==0: #Not nice but works. Some lines don't have an odd number of characters and script implodes. 
-			if all(char in string.printable for char in binascii.unhexlify(xor)): #Saves us from skimming through ugly lines
-				
-				print >>f, "XOR AGAINST "+str(hex(i))+": " + binascii.unhexlify(xor) #outputs to file. Comfier.
-	
-f.close()
+	if all(char in string.printable for char in binascii.unhexlify(xor)):
+		print "XOR AGAINST "+str(hex(possibleKey))+": " + binascii.unhexlify(xor)
 
-#SOLUCION:
+#SOLUTION:
 #XOR AGAINST 0x58: Cooking MC's like a pound of bacon
---
+++ trunk/freq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+import string
+rootdir = 'txts'
+frequencies = {}
+
+def letterOccurrances(aString): 
+    for character in aString:
+    	character=character.lower()
+	    if character in string.printable:
+	        if character not in frequencies.keys():
+	            frequencies[character]=0
+	        frequencies[character] += 1
+    return
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        #print os.path.join(subdir, file)
+
+        f=open(os.path.join(subdir, file))
+        letterOccurrances(f.read())
+        #print frequencies.keys()
+        f.close()
+
+freq=sorted(frequencies.iteritems(),key=lambda tup: tup[1],reverse=True)
+print freq
